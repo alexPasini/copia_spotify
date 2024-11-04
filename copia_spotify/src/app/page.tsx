27ab8c9e3923ee4d08c214b5268102aafc86ea21
Alex_Pasini <alexantoiopasini20@gmail.com>
2024-11-04 16:46:44 -0300

    first commit

---
+++ copia_spotify/src/app/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <div className="flex">
+        <aside>
+          sidebar
+        </aside>
+        <main className="flex-1">
+          main
+        </main>
+      </div>
+      <footer>
+        footer
+      </footer>
+    </div>
+  );
+}
