4e5aefc7a3c8b022997bee60461ae215984290d9
Alex_Pasini <alexantoiopasini20@gmail.com>
2024-12-19 17:20:56 -0300

    update

--- copia_spotify/src/app/page.tsx
+++ copia_spotify/src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="flex flex-col h-screen">
       <div className="flex">
         <aside>
           sidebar
